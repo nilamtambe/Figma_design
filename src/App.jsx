7bce41fdfca45f7d43c215ea0f50a8de502fe290
Nilam Tambe <111958530+nilamtambe@users.noreply.github.com>
2025-07-02 18:51:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Routes  from './component/Routes/index';
-import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from './component/Navbar/Navbar'
-import Hero from './component/Hero/Hero';
 import About from './component/About/About';
 import Services from './component/Services/Services';
-import Testimonial from './component/Testimonial/Testimonial';
 import Footer from './component/Footer/Footer';
 import Banner from './component/Banner/Banner';
 
@@ -15,20 +11,7 @@ import Banner from './component/Banner/Banner';
 const App = () => {
   return (
      
-    // <div className='overflow-x-hidden'>
-    //   <Router >
-    //     <Routes />
-    //   <Navbar />
-    //   <Hero />
-    //   <About />
-    //   <Footer />
-    //   <Services/>
-    //   <Banner />
-    //   <Testimonial />
-      
-    //     </Router>
-        
-    // </div>
+   
     <>
 <Navbar />         
 <div className="overflow-x-hidden" />  
@@ -62,23 +45,6 @@ export default App
 
 
 
-// import React from 'react';
-// import Navbar from './components/Navbar/Navbar';
-// import Hero from './components/Hero/Hero';
-// import About from './components/About/About';
-// import Services from './components/Services/Services';
 
 
-// const App = () => {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Hero />
-//       <About />
-//       <Services />
-//     </div>
-//   )
-// }
-
-// export default App
 
